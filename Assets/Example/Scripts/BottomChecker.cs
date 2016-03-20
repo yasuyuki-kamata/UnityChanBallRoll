c4de632e7ca982ed4d963e9cd4d7d21e0f042b88
Makoto Itoh <makoto@unity3d.com>
2016-03-20 21:12:09 +0900

    Retryの追加

--- Assets/Example/Scripts/BottomChecker.cs
+++ Assets/Example/Scripts/BottomChecker.cs
@@ -3,7 +3,22 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class BottomChecker : MonoBehaviour {
-	void OnTriggerEnter(Collider other) {
+	public GameObject retryButton;
+	public myCamCon camCon;
+
+	void Start()
+	{
+		retryButton.SetActive(false);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		retryButton.SetActive(true);
+		camCon.isLookAt = true;
+	}
+
+	public void Retry()
+	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 	 
--- Assets/Example/Scripts/myCamCon.cs
+++ Assets/Example/Scripts/myCamCon.cs
@@ -5,6 +5,7 @@ public class myCamCon : MonoBehaviour {
 
 	public GameObject player;
 	private Vector3 offset;
+	public bool isLookAt;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +14,10 @@ public class myCamCon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		this.transform.position = player.transform.position + offset;
+		if ( isLookAt ) {
+			this.transform.LookAt( player.transform.position );
+		} else {
+			this.transform.position = player.transform.position + offset;
+		}
 	}
 }
