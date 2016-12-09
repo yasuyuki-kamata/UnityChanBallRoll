8c1598f7291bd623bcca35161010e9544bd160c9
Yasuyuki Kamata <yasuyuki@unity3d.com>
2016-12-09 12:31:29 +0900

    Add NCMB SDK and UI

---
+++ Assets/LoginChecker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginChecker : MonoBehaviour
+{
+	[SerializeField] GameObject BeforeLoginUIPanel;
+	[SerializeField] GameObject AfterLoginUIPanel;
+
+	[SerializeField] bool isDebug;
+
+	public void DisplayNCMBPanelBeforeLogin()
+	{
+		BeforeLoginUIPanel.SetActive (true);
+		AfterLoginUIPanel.SetActive (false);
+	}
+
+	public void DisplayNCMBPanelAfterLogin()
+	{
+		BeforeLoginUIPanel.SetActive (false);
+		AfterLoginUIPanel.SetActive (true);
+	}
+
+	void OnGUI ()
+	{
+		if (isDebug) {
+			if (GUILayout.Button ("Login")) {
+				DisplayNCMBPanelAfterLogin ();
+			}
+			if (GUILayout.Button ("Logout")) {
+				DisplayNCMBPanelBeforeLogin ();
+			}
+		}
+	}
+}
